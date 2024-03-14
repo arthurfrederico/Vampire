8444962257775f1af8846c03751b94555be48f13
arthurfrederico <70981835+arthurfrederico@users.noreply.github.com>
2024-03-14 19:11:03 -0300

    Add files via upload

---
+++ Killfeed/DataStore.cs
@@ -0,0 +1,269 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using Backtrace.Unity.Common;
+using Bloodstone.API;
+using Il2CppSystem.Linq;
+using ProjectM;
+using ProjectM.Network;
+using Unity.Collections;
+using Unity.Mathematics;
+using UnityEngine.Jobs;
+using Random = System.Random;
+
+namespace Killfeed;
+public class DataStore
+{
+	public record struct PlayerStatistics(ulong SteamId, string LastName, int Kills, int Deaths, int CurrentStreak,
+		int HighestStreak, string LastClanName)
+	{
+		// lol yikes
+		private static string SafeCSVName(string s) => s.Replace(",", "");
+
+		public string ToCsv() => $"{SteamId},{SafeCSVName(LastName)},{Kills},{Deaths},{CurrentStreak},{HighestStreak},{SafeCSVName(LastClanName)}";
+
+		public static PlayerStatistics Parse(string csv)
+		{
+			// intentionally naieve and going to blow up so I'll catch it and not get an object for that player and log it
+			var split = csv.Split(',');
+			return new PlayerStatistics()
+			{
+				SteamId = ulong.Parse(split[0]),
+				LastName = split[1],
+				Kills = int.Parse(split[2]),
+				Deaths = int.Parse(split[3]),
+				CurrentStreak = int.Parse(split[4]),
+				HighestStreak = int.Parse(split[5]),
+				LastClanName = split.Length > 6 ? split[6] : ""
+			};
+		}
+	}
+
+	public record struct EventData(ulong VictimId, ulong KillerId, float3 Location, long Timestamp)
+	{
+		public string ToCsv() => $"{VictimId},{KillerId},{Location.x},{Location.y},{Location.z},{Timestamp}";
+
+		public static EventData Parse(string csv)
+		{
+			var split = csv.Split(',');
+			return new EventData()
+			{
+				VictimId = ulong.Parse(split[0]),
+				KillerId = ulong.Parse(split[1]),
+				Location = new float3(float.Parse(split[2]), float.Parse(split[3]), float.Parse(split[4])),
+				Timestamp = long.Parse(split[5])
+			};
+		}
+	}
+
+	public static List<EventData> Events = new();
+	public static Dictionary<ulong, PlayerStatistics> PlayerDatas = new();
+
+	private static readonly Random _rand = new();
+	private static EventData GenerateTestEvent() => new()
+	{
+		VictimId = (ulong)_rand.NextInt64(),
+		KillerId = (ulong)_rand.NextInt64(),
+		Location = new float3((float)_rand.NextDouble(), (float)_rand.NextDouble(), (float)_rand.NextDouble()),
+		Timestamp = DateTime.UtcNow.AddMinutes(_rand.Next(-10000, 10000)).Ticks
+	};
+
+	public static void GenerateNTestData(int count)
+	{
+		for (var i = 0; i < count; i++)
+		{
+			Events.Add(GenerateTestEvent());
+		}
+	}
+
+	private const string EVENTS_FILE_NAME = "events.v1.csv";
+	private const string EVENTS_FILE_PATH = $"BepInEx/config/Killfeed/{EVENTS_FILE_NAME}";
+
+	private const string STATS_FILE_NAME = "stats.v1.csv";
+	private const string STATS_FILE_PATH = $"BepInEx/config/Killfeed/{STATS_FILE_NAME}";
+
+	public static void WriteToDisk()
+	{
+		var dir = Path.GetDirectoryName(EVENTS_FILE_PATH);
+		if (!Directory.Exists(dir))
+		{
+			Directory.CreateDirectory(dir);
+		}
+
+		// TODO: Ideally this appends events and is smarter
+		using StreamWriter eventsFile = new StreamWriter(EVENTS_FILE_PATH, append: false);
+		foreach (var eventData in Events)
+		{
+			eventsFile.WriteLine(eventData.ToCsv());
+		}
+
+		using StreamWriter statsFile = new StreamWriter(STATS_FILE_PATH, append: false);
+		foreach (var playerData in PlayerDatas.Values)
+		{
+			statsFile.WriteLine(playerData.ToCsv());
+		}
+	}
+
+	public static void LoadFromDisk()
+	{
+		// can't think of how they would not be newed but let's be sure we don't duplicate data
+		Events.Clear();
+		PlayerDatas.Clear();
+
+		// let's assume maybe it can be empty or not exist and we don't care
+		LoadEventData();
+		LoadPlayerData();
+	}
+
+	private static void LoadEventData()
+	{
+		if (!File.Exists(EVENTS_FILE_PATH))
+		{
+			return;
+		}
+		using StreamReader eventsFile = new StreamReader(EVENTS_FILE_PATH);
+		while (!eventsFile.EndOfStream)
+		{
+			var line = eventsFile.ReadLine();
+			if (string.IsNullOrWhiteSpace(line)) continue;
+			try
+			{
+				Events.Add(EventData.Parse(line));
+			}
+			catch (Exception)
+			{
+				Plugin.Logger.LogError($"Failed to parse event line: \"{line}\"");
+			}
+		}
+	}
+
+	private static void LoadPlayerData()
+	{
+		if (!File.Exists(STATS_FILE_PATH)) return;
+		using StreamReader statsFile = new StreamReader(STATS_FILE_PATH);
+		while (!statsFile.EndOfStream)
+		{
+			var line = statsFile.ReadLine();
+			if (string.IsNullOrWhiteSpace(line)) continue;
+			try
+			{
+				var playerData = PlayerStatistics.Parse(line);
+				if (PlayerDatas.TryGetValue(playerData.SteamId, out PlayerStatistics data))
+				{
+					Plugin.Logger.LogWarning($"Duplicate player data found, overwriting {data} with {playerData}");
+				}
+				PlayerDatas[playerData.SteamId] = playerData;
+			}
+			catch (Exception)
+			{
+				Plugin.Logger.LogError($"Failed to parse player line: \"{line}\"");
+			}
+		}
+	}
+
+	public static void RegisterKillEvent(PlayerCharacter victim, PlayerCharacter killer, float3 location)
+	{
+		var victimUser = victim.UserEntity.Read<User>();
+		var killerUser = killer.UserEntity.Read<User>();
+
+		var newEvent = new EventData(victimUser.PlatformId, killerUser.PlatformId, location, DateTime.UtcNow.Ticks);
+
+		Events.Add(newEvent);
+
+
+		PlayerStatistics UpsertName(ulong steamId, string name, string clanName)
+		{
+			if (PlayerDatas.TryGetValue(steamId, out var player))
+			{
+				player.LastName = name;
+				player.LastClanName = clanName;
+				PlayerDatas[steamId] = player;
+			}
+			else
+			{
+				PlayerDatas[steamId] = new PlayerStatistics() { LastName = name, SteamId = steamId, LastClanName = clanName };
+			}
+
+			return PlayerDatas[steamId];
+		}
+
+
+		var victimData = UpsertName(victimUser.PlatformId, victimUser.CharacterName.ToString(), victim.SmartClanName.ToString());
+		var killerData = UpsertName(killerUser.PlatformId, killerUser.CharacterName.ToString(), killer.SmartClanName.ToString());
+
+		RecordKill(killerUser.PlatformId);
+		var lostStreak = RecordDeath(victimUser.PlatformId);
+
+		AnnounceKill(victimData, killerData, lostStreak);
+
+		// TODO: Very bad, but going to save to disk each kill for nice hiccup of lag
+		// while this is naieve and whole file, in append or WAL this might be better
+		WriteToDisk();
+	}
+	
+	private static void AnnounceKill(PlayerStatistics victimUser, PlayerStatistics killerUser, int lostStreakAmount)
+	{
+		if (!Settings.AnnounceKills) return;
+
+		var victimName = Markup.Highlight(victimUser.LastName);
+		var killerName = Markup.Highlight(killerUser.LastName);
+
+		var message = lostStreakAmount > Settings.AnnounceKillstreakLostMinimum
+			? $"{killerName} finalizou a kill streak de {victimName} de {Markup.Secondary(lostStreakAmount) } vitimas!"
+			: $"{killerName} matou {victimName}!";
+
+		var killMsg = killerUser.CurrentStreak switch
+		{
+			5 => $"<size=18>{killerName} ta numa killing spree!",
+			10 => $"<size=19>{killerName}  rampage!",
+			15 => $"<size=20>{killerName}  dominating!",
+			20 => $"<size=21>{killerName}  unstoppable!",
+			25 => $"<size=22>{killerName}  godlike!",
+			30 => $"<size=24>{killerName}  WICKED SICK!",
+			_ => null
+		};
+
+		ServerChatUtils.SendSystemMessageToAllClients(VWorld.Server.EntityManager, Markup.Prefix + message);
+		
+		if (!string.IsNullOrEmpty(killMsg) && Settings.AnnounceKillstreak)
+		{
+			ServerChatUtils.SendSystemMessageToAllClients(VWorld.Server.EntityManager, Markup.Prefix + killMsg);
+		}
+	}
+
+	private static int RecordDeath(ulong platformId)
+	{
+		var lostStreak = 0;
+		if (PlayerDatas.TryGetValue(platformId, out var player))
+		{
+			player.Deaths++;
+
+			lostStreak = player.CurrentStreak;
+			player.CurrentStreak = 0;
+
+			PlayerDatas[platformId] = player;
+		}
+		else
+		{
+			PlayerDatas[platformId] = new PlayerStatistics() { Deaths = 1, SteamId = platformId };
+		}
+
+		return lostStreak;
+	}
+
+	private static void RecordKill(ulong steamId)
+	{
+		if (PlayerDatas.TryGetValue(steamId, out var player))
+		{
+			player.Kills++;
+			player.CurrentStreak++;
+			player.HighestStreak = math.max(player.HighestStreak, player.CurrentStreak);
+			PlayerDatas[steamId] = player;
+		}
+		else
+		{
+			PlayerDatas[steamId] = new PlayerStatistics() { Kills = 1, CurrentStreak = 1, HighestStreak = 1, SteamId = steamId };
+		}
+	}
+}
---
+++ Killfeed/VampireDownedHook.cs
@@ -0,0 +1,71 @@
+﻿using Bloodstone.API;
+using HarmonyLib;
+using ProjectM;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Transforms;
+
+namespace Killfeed;
+
+[HarmonyPatch(typeof(VampireDownedServerEventSystem), nameof(VampireDownedServerEventSystem.OnUpdate))]
+public static class VampireDownedHook
+{
+    public static void Prefix(VampireDownedServerEventSystem __instance)
+    {
+        var downedEvents = __instance.__OnUpdate_LambdaJob0_entityQuery.ToEntityArray(Allocator.Temp);
+        foreach (var entity in downedEvents)
+        {
+            ProcessVampireDowned(entity);
+        }
+    }
+
+    private static void ProcessVampireDowned(Entity entity)
+    {
+        
+        if(!VampireDownedServerEventSystem.TryFindRootOwner(entity, 1, VWorld.Server.EntityManager, out var victimEntity))
+        {
+            Plugin.Logger.LogMessage("Não foi possível declarar a vitima");
+            return;
+        }
+		
+		var downBuff = entity.Read<VampireDownedBuff>();
+
+		
+		if (!VampireDownedServerEventSystem.TryFindRootOwner(downBuff.Source, 1, VWorld.Server.EntityManager, out var killerEntity))
+		{
+			Plugin.Logger.LogMessage("Não foi possivel declarar a causa da morte.");
+			return;
+		}
+		
+		var victim = victimEntity.Read<PlayerCharacter>();
+
+		Plugin.Logger.LogMessage($"{victim.Name} é a vitima");
+		var unitKiller = killerEntity.Has<UnitLevel>();
+		
+		if (unitKiller)
+        {
+			Plugin.Logger.LogInfo($"{victim.Name} foi morto por uma criatura.");
+            return;
+		}
+
+		var playerKiller = killerEntity.Has<PlayerCharacter>();
+
+		if (!playerKiller)
+        {
+            Plugin.Logger.LogWarning($"Deixa MathPonte saber que tem mais uma morte e não foi pvp ou mob.");
+            return;
+        }
+		
+		var killer = killerEntity.Read<PlayerCharacter>();
+
+		if (killer.UserEntity == victim.UserEntity)
+        {
+            Plugin.Logger.LogInfo($"{victim.Name} se matou.");
+            return;
+        }
+		
+        var location = victimEntity.Read<LocalToWorld>();
+		
+        DataStore.RegisterKillEvent(victim, killer, location.Position);
+    }
+}
